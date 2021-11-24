07f23f583b2fedf5bd4e14a38a83d70cb461a11a
cestwc <80936226+cestwc@users.noreply.github.com>
2021-11-24 18:10:20 +0800

    Update label.py

--- label.py
+++ label.py
@@ -51,104 +51,104 @@ def alike(tokenizer):
 	return {vocab[k]:v for k, v in tokens.items()}
 
 def enlarge(ids):
-    A = set(ids)
-    B = set().union(*[interchangeable[a] for a in A])
-    return B, B - A
+	A = set(ids)
+	B = set().union(*[interchangeable[a] for a in A])
+	return B, B - A
 
 def punkts(tokenizer):
-    punkt = set()
-    G = tokenizer.convert_ids_to_tokens(1437)
+	punkt = set()
+	G = tokenizer.convert_ids_to_tokens(1437)
 
-    for k,v in tokenizer.vocab.items():
-        if not any(letter.isalnum() for letter in k.replace(G, '')):
-            punkt.add(v)
-    
-    return punkt
+	for k,v in tokenizer.vocab.items():
+		if not any(letter.isalnum() for letter in k.replace(G, '')):
+			punkt.add(v)
+
+	return punkt
 
 def confine(input_ids, highlights_ids):
-    W = enlarge(highlights_ids)[0] - {1}
+	W = enlarge(highlights_ids)[0] - {1}
 
-    variables = []
-    labels = []
-    for i, x in enumerate(input_ids):
-        if x in punctuation:
-            labels.append(1)
-        else:
-            labels.append(0)
-            if x in W:
-                variables.append(i)
+	variables = []
+	labels = []
+	for i, x in enumerate(input_ids):
+		if x in punctuation:
+			labels.append(1)
+		else:
+			labels.append(0)
+			if x in W:
+				variables.append(i)
 
-    return labels, variables
+	return labels, variables
 
 def tokenize(e):
-    article = tokenizer(e['article'], truncation=True)
-    highlights = tokenizer(e['highlights'], truncation=True)
-    article['highlight_ids'] = highlights['input_ids']
-    labels, confined = zip(*list(map(confine, article['input_ids'], article['highlight_ids'])))
-    article['confined'] = list(confined)
-    article['labels'] = list(labels)
-    return article
+	article = tokenizer(e['article'], truncation=True)
+	highlights = tokenizer(e['highlights'], truncation=True)
+	article['highlight_ids'] = highlights['input_ids']
+	labels, confined = zip(*list(map(confine, article['input_ids'], article['highlight_ids'])))
+	article['confined'] = list(confined)
+	article['labels'] = list(labels)
+	return article
 
 
 def obj_func_single(inputs, variables):
-    mask = inputs['labels'].astype(bool)
-    mask[inputs['confined']] = variables
-    prediction = tokenizer.decode(inputs['input_ids'][mask], skip_special_tokens=True, clean_up_tokenization_spaces=False)
-    # prediction = ' '.join(prediction.translate(table_).split())
-    ROUGE = scorer.score(inputs['highlights'], prediction)
-    return ROUGE['rouge1'].fmeasure * ROUGE['rouge2'].fmeasure * ROUGE['rougeL'].fmeasure
+	mask = inputs['labels'].astype(bool)
+	mask[inputs['confined']] = variables
+	prediction = tokenizer.decode(inputs['input_ids'][mask], skip_special_tokens=True, clean_up_tokenization_spaces=False)
+	# prediction = ' '.join(prediction.translate(table_).split())
+	ROUGE = scorer.score(inputs['highlights'], prediction)
+	return ROUGE['rouge1'].fmeasure * ROUGE['rouge2'].fmeasure * ROUGE['rougeL'].fmeasure
 
 def obj_func(inputs, x):
-    return - np.array(list(map(lambda b: obj_func_single(inputs, b), x)))
+	return - np.array(list(map(lambda b: obj_func_single(inputs, b), x)))
 
 class MyProblem(Problem):
 
-    def __init__(self, inputs):
-        self.inputs = inputs
-        super().__init__(n_var=len(self.inputs['confined']), n_obj=1, n_constr=0, xl=0, xu=1, type_var=int)
+	def __init__(self, inputs):
+		self.inputs = inputs
+		super().__init__(n_var=len(self.inputs['confined']), n_obj=1, n_constr=0, xl=0, xu=1, type_var=int)
 
-    def _evaluate(self, x, out, *args, **kwargs):
-        out["F"] = obj_func(self.inputs, x)
+	def _evaluate(self, x, out, *args, **kwargs):
+		out["F"] = obj_func(self.inputs, x)
 
 def highlight(e):
-    e['labels'][e['confined']] = minimize(MyProblem(e),
-                                         algorithm,
-                                         ('n_gen', 90),
-                                         verbose=False).X.astype(int)
-    return e
+	e['labels'][e['confined']] = minimize(MyProblem(e),
+										 algorithm,
+										 ('n_gen', 90),
+										 verbose=False).X.astype(int)
+	return e
 
 def main():
-    tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
+	tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
 
-    useful = lambda x: len(x['article']) > len(x['highlights']) + 500
+	useful = lambda x: len(x['article']) > len(x['highlights']) + 500
 
-    raw_data = load_from_disk(opt.dataPath)[opt.split].filter(useful).shard(opt.shard, opt.index)
+	raw_data = load_from_disk(opt.dataPath)[opt.split].filter(useful).shard(opt.shard, opt.index)
 
-    interchangeable = alike(tokenizer)
+	interchangeable = alike(tokenizer)
 
-    punctuation = punkts(tokenizer)
+	punctuation = punkts(tokenizer)
 
-    tokenized_data = raw_data.map(tokenize, batched=True)
+	tokenized_data = raw_data.map(tokenize, batched=True)
 
-    print(f"Number of training examples: {len(tokenized_data)}")
+	print(f"Number of training examples: {len(tokenized_data)}")
 
-    tokenized_data.set_format(type = 'numpy', columns=['input_ids', 'labels', 'confined', 'highlights'])
+	tokenized_data.set_format(type = 'numpy', columns=['input_ids', 'labels', 'confined', 'highlights'])
 
-    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+	scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
-    table_ = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+	table_ = str.maketrans(string.punctuation, ' '*len(string.punctuation))
 
-    algorithm = GA(
-        pop_size=28,
-        sampling=get_sampling("bin_random"),
-        crossover=get_crossover("bin_hux"),
-        mutation=get_mutation("bin_bitflip"),
-        eliminate_duplicates=True)
+	algorithm = GA(
+		pop_size=28,
+		sampling=get_sampling("bin_random"),
+		crossover=get_crossover("bin_hux"),
+		mutation=get_mutation("bin_bitflip"),
+		eliminate_duplicates=True)
 
-    labelled_data = tokenized_data.map(highlight, batched=False)
+	labelled_data = tokenized_data.map(highlight, batched=False)
 
-    labelled_data.save_to_disk(f"{opt.save}/{opt.split}_{opt.shard}_{opt.index}")
+	labelled_data.save_to_disk(f"{opt.save}/{opt.split}_{opt.shard}_{opt.index}")
 
 
 if __name__ == "__main__":
-    main()
+	main()
