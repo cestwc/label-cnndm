2cc61248cd02b05b217c5d2505b82e94f9dbf236
cestwc <80936226+cestwc@users.noreply.github.com>
2021-12-02 17:07:56 +0800

    Update label_ngrams.py

--- label_ngrams.py
+++ label_ngrams.py
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser(description="cnn_dailymail")
 # parser.add_argument("--shard", type=int, default=128, help="divide the dataset into")
 # parser.add_argument("--index", type=int, default=0, help="which partition")
 parser.add_argument("--dataPath", type=str, default='cnn_dailymail', help='path of files to process')
-parser.add_argument("--save", type=str, default='/content/drive/My Drive/Colab Notebooks/cnn_dailymail_ngrams', help='path to save processed data')
+parser.add_argument("--save", type=str, default='cnn_dailymail_ngrams', help='path to save processed data')
 # parser.add_argument('--shard', action="store_true", help='use DnCNN as reference?')
 # parser.add_argument("--preprocess", type=bool, default=False, help='run prepare_data or not')
 # parser.add_argument("--lr", type=float, default=1e-3, help="Initial learning rate")
@@ -114,7 +114,7 @@ def main():
 	
 	for k in cnn_dailymail:
 
-		cnn_dailymail[k] = cnn_dailymail[k].map(tokenize, batched=True)
+		cnn_dailymail[k] = cnn_dailymail[k].shard(3000, 0).map(tokenize, batched=True)
 
 		cnn_dailymail[k] = cnn_dailymail[k].map(unigrams, batched=False)
 
